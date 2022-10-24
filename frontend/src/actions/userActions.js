324ad7c04ae3e97de0810089859eb10852ad6c59
challelign <chalie2123@gmail.com>
2022-10-24 23:44:09 +0300

    Login UserReducer& action, Login User Component done

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+import {
+    LOGIN_REQUEST,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    CLEAR_ERRORS
+} from '../constants/userConstants.js'
+
+
+//login
+export const login = (email, password) => async (dispatch) => {
+    try {
+
+        dispatch({
+            type: LOGIN_REQUEST
+        })
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        const {data} = await axios.post('/api/v1/login', {
+            email, password
+        }, config)
+
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: data.user
+        })
+    } catch (error) {
+        dispatch({
+            type: LOGIN_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+
+
+// CLEAR_ERRORS
+
+export const clearErrors = () =>async(dispatch) =>{
+    dispatch ({
+        type:CLEAR_ERRORS
+    })
+}
