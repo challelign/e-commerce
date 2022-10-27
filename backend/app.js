488574147860caf76a831990e294146eda88505f
Challelign <chalie2123@gmail.com>
2022-10-27 15:14:46 +0300

    User Register and user Avater

--- backend/app.js
+++ backend/app.js
@@ -1,38 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+const cookieParser = require("cookie-parser");
 
-const cookieParser = require('cookie-parser')
+const bodyparser = require("cookie-parser");
+const cloudinary = require("cloudinary");
 
-
-const errorMiddleware = require('./middlewares/errors')
-
-
+const errorMiddleware = require("./middlewares/errors");
 
 app.use(express.json());
 
-app.use(cookieParser())
-
-
-
-
+app.use(cookieParser());
 
 // Import all routes
 
-const products = require('./routes/product');
-const auth = require('./routes/auth');
-const order = require('./routes/order');
+const products = require("./routes/product");
+const auth = require("./routes/auth");
+const order = require("./routes/order");
 
+app.use("/api/v1", products);
 
-app.use('/api/v1', products);
+app.use("/api/v1", auth);
 
-app.use('/api/v1', auth);
-
-app.use('/api/v1', order);
-
+app.use("/api/v1", order);
 
 // Middleware to handler error
 app.use(errorMiddleware);
-
 
 module.exports = app;
